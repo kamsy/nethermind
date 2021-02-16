b69ea8337f6a732d9a08bdf51021423acb7a1332
kamsy <olekakamsy@gmail.com>
2021-02-16 13:13:46 +0100

    done with login

---
+++ src/pages/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../style/login.scss";
+import { connect } from "react-redux";
+import loginUser from "../actionCreators/loginUser";
+import { history } from "../App";
+
+const Login = ({ dispatchLogin }) => {
+    const handleLogin = () => {
+        dispatchLogin();
+        history.push({ pathname: "/cats" });
+    };
+    return (
+        <div className="login-page">
+            <form className="form">
+                <h3>Login</h3>
+                <input
+                    value="olekakamsy@gmail.com"
+                    type="email"
+                    readOnly
+                    placeholder="Enter your email"
+                />
+                <input
+                    value="netherminds"
+                    readOnly
+                    type="password"
+                    placeholder="Enter your password"
+                />
+
+                <button
+                    type="button"
+                    className="cstm-btn"
+                    onClick={handleLogin}>
+                    <span>Log in</span>
+                </button>
+            </form>
+        </div>
+    );
+};
+
+const mapDispatchToProps = dispatch => ({
+    dispatchLogin: () => dispatch(loginUser())
+});
+
+export default connect(null, mapDispatchToProps)(Login);
